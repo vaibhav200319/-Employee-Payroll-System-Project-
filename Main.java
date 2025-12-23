403c1991c307b26586621a27d3beb0132ae83c49
VaibhavYelis19 <vaibhavyelis@gmail.com>
2025-12-23 18:21:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,118 @@
+package Employeemanagementsystem;
+
+import java.util.ArrayList;
+
+// Abstract Employee class
+abstract class Employee {
+    private String name;
+    private int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public abstract double calculateSalary();
+
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", id=" + id + ", salary=" + calculateSalary() + "]";
+    }
+}
+
+// Full-time employee
+class FullTimeEmployee extends Employee {
+    private double monthlySalary;
+
+    public FullTimeEmployee(String name, int id, double monthlySalary) {
+        super(name, id);
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return monthlySalary;
+    }
+}
+
+// Part-time employee
+class PartTimeEmployee extends Employee {
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate) {
+        super(name, id);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+}
+
+// Payroll system
+class PayrollSystem {
+    private ArrayList<Employee> employeeList;
+
+    public PayrollSystem() {
+        employeeList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public void removeEmployee(int id) {
+        Employee employeeToRemove = null;
+
+        for (Employee employee : employeeList) {
+            if (employee.getId() == id) {
+                employeeToRemove = employee;
+                break;
+            }
+        }
+
+        if (employeeToRemove != null) {
+            employeeList.remove(employeeToRemove);
+        }
+    }
+
+    public void displayEmployees() {
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+
+        PayrollSystem payrollSystem = new PayrollSystem();
+
+        FullTimeEmployee emp1 = new FullTimeEmployee("Vaibhav", 1, 70000);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Mayur", 5, 40, 450);
+
+        payrollSystem.addEmployee(emp1);
+        payrollSystem.addEmployee(emp2);
+
+        System.out.println("Initial Employee Details:");
+        payrollSystem.displayEmployees();
+
+        System.out.println("\nRemoving Employee with ID 5...");
+        payrollSystem.removeEmployee(5);
+
+        System.out.println("\nRemaining Employee Details:");
+        payrollSystem.displayEmployees();
+    }
+}
